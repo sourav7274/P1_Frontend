46e63a049560602451f35cc55764d2f158004b1f
Sourav <samjhonson932@gmail.com>
2025-05-08 20:41:40 +0530

    Login_page_VIs_FIX

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,33 +6,33 @@ const savedUser = sessionStorage.getItem("user");
 export const localUser =
   savedUser && savedUser != "undefined" ? JSON.parse(savedUser) : null;
 
-export const getUser = createAsyncThunk("get/user", async (data) => {
-  const response = await axios.post(
-    `https://p1-backend-pqsg.onrender.com/login`,
-    data
-  );
-  const result = await response.data;
-  localStorage.setItem("token", result.token);
-  sessionStorage.setItem("user", JSON.stringify(result.user));
-  return response.data.user;
-});
+export const getUser = createAsyncThunk(
+  "get/user",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(`http://localhost:5000/login`, data);
+      const result = await response.data;
+      localStorage.setItem("token", result.token);
+      sessionStorage.setItem("user", JSON.stringify(result.user));
+      return response.data.user;
+    } catch (err) {
+      const message = err.response?.data?.message || "Unknown error";
+      return rejectWithValue(message);
+    }
+  }
+);
 export const getUserByID = createAsyncThunk("get/userId", async (id) => {
-  const response = await axios.get(
-    `https://p1-backend-pqsg.onrender.com/userId/${id}`
-  );
+  const response = await axios.get(`http://localhost:5000/userId/${id}`);
   return response.data.user[0];
 });
 export const makeOrder = createAsyncThunk("/post/order", async (data) => {
-  const response = await axios.post(
-    `https://p1-backend-pqsg.onrender.com/order`,
-    data
-  );
+  const response = await axios.post(`http://localhost:5000/order`, data);
   // console.log(response.data);
   return response.data.user;
 });
 
 // export const getOrders = createAsyncThunk("get/orders", async (id) => {
-//   const response = await axios.get(`https://p1-backend-pqsg.onrender.com/orderHistory/${id}`);
+//   const response = await axios.get(`http://localhost:5000//orderHistory/${id}`);
 //   // console.log(response.data);
 // });
 
@@ -41,7 +41,7 @@ export const addtoCartFWish = createAsyncThunk(
   async ({ id, data }) => {
     console.log(data);
     const response = await axios.put(
-      `https://p1-backend-pqsg.onrender.com/${id}/cartFWish`,
+      `http://localhost:5000/${id}/cartFWish`,
       data
     );
     return response.data.user;
@@ -51,10 +51,7 @@ export const addtoCartFWish = createAsyncThunk(
 export const deleteItemFCart = createAsyncThunk(
   "delete/cart",
   async ({ id, data }) => {
-    const response = await axios.put(
-      `https://p1-backend-pqsg.onrender.com/${id}/cart`,
-      data
-    );
+    const response = await axios.put(`http://localhost:5000/${id}/cart`, data);
     // console.log(response.data);
     return response.data.user;
   }
@@ -65,30 +62,36 @@ export const deleteItemFWishlist = createAsyncThunk(
   async ({ id, data }) => {
     console.log(data);
     const response = await axios.put(
-      `https://p1-backend-pqsg.onrender.com/${id}/wishlist`,
+      `http://localhost:5000/${id}/wishlist`,
       data
     );
     return response.data.user;
   }
 );
 
-export const postUser = createAsyncThunk("post/user", async (data) => {
-  const response = await axios.post(
-    "https://p1-backend-pqsg.onrender.com/user",
-    data
-  );
-  const result = await response.data;
-  console.log(result.message);
-  return result.newUser;
-});
+export const postUser = createAsyncThunk(
+  "post/user",
+  async (data, { rejectWithValue }) => {
+    try {
+      const response = await axios.post("http://localhost:5000/user", data);
+      const result = await response.data;
+      // console.log(result);
+      return result.newUser;
+    } catch (err) {
+      const message =
+        err.response && err.response.data && err.response.data.err
+          ? err.response.data.err
+          : "Unknown error";
+
+      return rejectWithValue(message);
+    }
+  }
+);
 
 export const updateUser = createAsyncThunk(
   "update/user",
   async ({ id, data }) => {
-    const response = await axios.put(
-      `https://p1-backend-pqsg.onrender.com/user/${id}`,
-      data
-    );
+    const response = await axios.put(`http://localhost:5000/user/${id}`, data);
     const result = await response.data;
     // console.log(result.user)
     return result.user;
@@ -99,7 +102,7 @@ export const addToWishlist = createAsyncThunk(
   "add/wish",
   async ({ id, data }) => {
     const response = await axios.post(
-      `https://p1-backend-pqsg.onrender.com/user/${id}/wishlist`,
+      `http://localhost:5000/user/${id}/wishlist`,
       data
     );
     // console.log(response.data.user)
@@ -109,7 +112,7 @@ export const addToWishlist = createAsyncThunk(
 
 export const addToCart = createAsyncThunk("add/cart", async ({ id, data }) => {
   const response = await axios.post(
-    `https://p1-backend-pqsg.onrender.com/user/${id}/cart`,
+    `http://localhost:5000/user/${id}/cart`,
     data
   );
   return response.data.user;
@@ -120,7 +123,7 @@ export const deleteAddress = createAsyncThunk(
   async ({ id, addId }) => {
     // console.log(id, addId);
     const response = await axios.put(
-      `https://p1-backend-pqsg.onrender.com/deleteAddress/${id}`,
+      `http://localhost:5000/deleteAddress/${id}`,
       { addId }
     );
     // console.log(response.data.message);
@@ -134,7 +137,11 @@ const userSlice = createSlice({
     loading: null,
     error: null,
   },
-  reducers: {},
+  reducers: {
+    clearErrors: (state) => {
+      state.error = null;
+    },
+  },
   extraReducers: (builder) => {
     builder.addCase(getUser.pending, (state) => {
       state.loading = "loading";
@@ -146,10 +153,15 @@ const userSlice = createSlice({
     });
     builder.addCase(getUser.rejected, (state, action) => {
       state.loading = null;
-      state.error = action.error.message;
+      state.error = action.payload || "Generic message, invalid credentials";
     });
     builder.addCase(postUser.fulfilled, (state, action) => {
       state.user = action.payload;
+    });
+    builder.addCase(postUser.rejected, (state, action) => {
+      state.error =
+        action.payload ||
+        "Generic Email ID already exists in Database, try another email ID";
     });
     builder.addCase(getUserByID.fulfilled, (state, action) => {
       state.user = action.payload;
@@ -192,3 +204,4 @@ const userSlice = createSlice({
 });
 
 export default userSlice.reducer;
+export const { clearErrors } = userSlice.actions;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,10 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { getUser, postUser } from "../features/user/userSlice";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { clearErrors } from "../features/user/userSlice";
+import { motion, AnimatePresence, animate } from "framer-motion";
+
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const { error } = useSelector((state) => state.user);
+  // console.log(error)
   const [email, setMail] = useState("");
   const [password, setPasword] = useState("");
   const [user, setUser] = useState({
@@ -29,144 +35,203 @@ const Login = () => {
   const handleSign = async (e) => {
     e.preventDefault();
     const data = { email, password };
-    const result = await dispatch(getUser(data))
-    if(getUser.fulfilled.match(result))
-    {
-        navigate('/home')
+    const result = await dispatch(getUser(data));
+    if (getUser.fulfilled.match(result)) {
+      navigate("/home");
     }
-};
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch(clearErrors());
+    }, 3000);
+  }, [error]);
+
   return (
     <>
-      {isLogin ? (
-        <>
-          {/* Login Component */}
-          <div className="container ps-5 mt-5">
-            <div className="row">
-              <div className="col-md-6 offset-md-3">
-                <h2 className="text-center">Login</h2>
-                <form>
-                  <div className="mb-3">
-                    <label htmlFor="email" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      onChange={(e) => setMail(e.target.value)}
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      required
-                    />
+      <AnimatePresence mode="wait">
+        {isLogin ? (
+          <>
+            {/* Login Component */}
+            <motion.div
+              key="login"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="text-light bg-dark"
+            >
+              <div
+                style={{ height: "100vh", width: "100%" }}
+                className="container ps-5 pt-5"
+              >
+                <div className="row">
+                  <div className="col-md-6 offset-md-3">
+                    <h2 className="text-center">Login</h2>
+                    <form>
+                      <div className="mb-3">
+                        <label htmlFor="email" className="form-label">
+                          Email address
+                        </label>
+                        <input
+                          onChange={(e) => setMail(e.target.value)}
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          required
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="password" className="form-label">
+                          Password
+                        </label>
+                        <input
+                          onChange={(e) => setPasword(e.target.value)}
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          required
+                        />
+                      </div>
+                      <button
+                        onClick={(e) => handleSign(e)}
+                        type="submit"
+                        className="btn btn-light"
+                      >
+                        Login
+                      </button>
+                    </form>
                   </div>
-                  <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                      Password
-                    </label>
-                    <input
-                      onChange={(e) => setPasword(e.target.value)}
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      required
-                    />
-                  </div>
-                  <button
-                    onClick={(e) => handleSign(e)}
-                    type="submit"
-                    className="btn btn-primary"
-                  >
-                    Login
-                  </button>
-                </form>
-              </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-md-6 offset-md-3 text-center">
-                <p>
-                  Don't have an account?{" "}
-                  <button
-                    onClick={() => setIsLogin(!isLogin)}
-                    className="bg-primary"
-                  >
-                    Register Here
-                  </button>
-                </p>
-              </div>
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          {/* Register Component */}
-          <div className="container ps-5 mt-5">
-            <div className="row">
-              <div className="col-md-6 offset-md-3">
-                <h2 className="text-center">Login</h2>
-                <form>
-                  <div className="mb-3">
-                    <label className="form-label " htmlFor="name">
-                      Name
-                    </label>
-                    <input
-                      type="text"
-                      name="name"
-                      value={user.name}
-                      onChange={handleChange}
-                      className="form-control mb-3"
-                    />
+                </div>
+                {error !== null && (
+                  <div
+                    style={{
+                      padding: "10px",
 
-                    <label htmlFor="email" className="form-label">
-                      Email address
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      onChange={handleChange}
-                      value={user.email}
-                      className="form-control mb-3"
-                      id="email"
-                      required
-                    />
-                  </div>
-                  <div className="mb-3">
-                    <label htmlFor="password" className="form-label">
-                      Password
-                    </label>
-                    <input
-                      name="password"
-                      onChange={handleChange}
-                      value={user.password}
-                      type="password"
-                      className="form-control"
-                      id="password"
-                      required
-                    />
-                  </div>
-                  <button
-                    onClick={(e) => handleRegisterSubmit(e)}
-                    type="submit"
-                    className="btn btn-primary"
+                      margin: "15px 0",
+
+                      color: "white",
+                      backgroundColor: "#dc3545", // red for error
+                      textAlign: "center",
+                      borderRadius: "5px",
+                    }}
                   >
-                    Register
-                  </button>
-                </form>
+                    {error}
+                  </div>
+                )}
+                <div className="row mt-3">
+                  <div className="col-md-6 offset-md-3 text-center">
+                    <p>
+                      Don't have an account?{" "}
+                      <button
+                        onClick={() => setIsLogin(!isLogin)}
+                        className="btn ms-3 btn-light"
+                      >
+                        Register Here
+                      </button>
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-            <div className="row mt-3">
-              <div className="col-md-6 offset-md-3 text-center">
-                <p>
-                  Already have an account?{" "}
-                  <button
-                    onClick={() => setIsLogin(!isLogin)}
-                    className="text-primary"
+            </motion.div>
+          </>
+        ) : (
+          <>
+            {/* Register Component */}
+            <motion.div
+              key="register"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className=""
+            >
+              <div className="container ps-5 pt-5">
+                <div className="row">
+                  <div className="col-md-6 offset-md-3">
+                    <h2 className="text-center">Register</h2>
+                    <form>
+                      <div className="mb-3">
+                        <label className="form-label " htmlFor="name">
+                          Name
+                        </label>
+                        <input
+                          type="text"
+                          name="name"
+                          value={user.name}
+                          onChange={handleChange}
+                          className="form-control mb-3"
+                        />
+
+                        <label htmlFor="email" className="form-label">
+                          Email address
+                        </label>
+                        <input
+                          type="email"
+                          name="email"
+                          onChange={handleChange}
+                          value={user.email}
+                          className="form-control mb-3"
+                          id="email"
+                          required
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <label htmlFor="password" className="form-label">
+                          Password
+                        </label>
+                        <input
+                          name="password"
+                          onChange={handleChange}
+                          value={user.password}
+                          type="password"
+                          className="form-control"
+                          id="password"
+                          required
+                        />
+                      </div>
+                      <button
+                        onClick={(e) => handleRegisterSubmit(e)}
+                        type="submit"
+                        className="btn btn-dark"
+                      >
+                        Register
+                      </button>
+                    </form>
+                  </div>
+                </div>
+                {error !== null && (
+                  <div
+                    style={{
+                      padding: "10px",
+
+                      margin: "15px 0",
+
+                      color: "white",
+                      backgroundColor: "#dc3545", // red for error
+                      textAlign: "center",
+                      borderRadius: "5px",
+                    }}
                   >
-                    Sign Here
-                  </button>
-                </p>
+                    {error}
+                  </div>
+                )}
+                <div className="row mt-3">
+                  <div className="col-md-6 offset-md-3 text-center">
+                    <p>
+                      Already have an account?{" "}
+                      <button
+                        onClick={() => setIsLogin(!isLogin)}
+                        className="btn btn-dark  "
+                      >
+                        Sign In Here
+                      </button>
+                    </p>
+                  </div>
+                </div>
               </div>
-            </div>
-          </div>
-        </>
-      )}
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
     </>
   );
 };
